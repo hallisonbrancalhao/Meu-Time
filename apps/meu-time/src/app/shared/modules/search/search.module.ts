0c38b680e2bdcfbdf39fdcfddb6a3f9cb04412e9
Hallison Brancalhão <hallison.o.b@gmail.com>
2023-05-28 01:43:26 -0300

    feat: base components

--- apps/meu-time/src/app/shared/modules/search/search.module.ts
+++ apps/meu-time/src/app/shared/modules/search/search.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TeamComponent } from './pages/team/team.component';
+import { ListComponent } from './list/list.component';
+import { FindComponent } from './find/find.component';
+import { HeaderComponent } from './header/header.component';
 import { SelectComponent } from './pages/select/select.component';
 import { SearchRoutingModule } from './search-routing.module';
 
 @NgModule({
-  declarations: [SelectComponent, TeamComponent],
+  declarations: [
+    SelectComponent,
+    TeamComponent,
+    HeaderComponent,
+    ListComponent,
+    FindComponent,
+  ],
   imports: [CommonModule, SearchRoutingModule],
 })
 export class SearchModule {}
