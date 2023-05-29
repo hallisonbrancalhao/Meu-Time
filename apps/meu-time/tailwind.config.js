9b61f16eff0bc11d593e9b871a7eabf9cde4cd48
Hallison Brancalhão <hallison.o.b@gmail.com>
2023-05-29 00:41:28 -0300

    style: select page styling

--- apps/meu-time/tailwind.config.js
+++ apps/meu-time/tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': {'max': '639px'},
+      }
+    },
   },
   plugins: [],
 };
