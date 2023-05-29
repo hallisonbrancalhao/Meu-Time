830dcafc2580350c26c537e32c4e61435a8132f7
Hallison Brancalhão <hallison.o.b@gmail.com>
2023-05-29 01:17:51 -0300

    feat: access key

--- apps/bff-pattern/src/auth/auth.service.ts
+++ apps/bff-pattern/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { IAuth } from './@types/auth.type';
 
 export class AuthService {
-  #key = '123';
+  #key = '4bdea2629dmsh648bf9a51aa0838p12d1ccjsn2ff47de99fff';
 
   validate(data: IAuth): boolean {
     return data.key === this.#key;
