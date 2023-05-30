6a7f67699489323551ff7227ffb66349bd2c8fc3
Hallison <hallison.o.b@gmail.com>
2023-05-30 14:31:02 -0300

    fix: request service statistics

--- apps/meu-time/src/app/shared/core/service/team/team.service.ts
+++ apps/meu-time/src/app/shared/core/service/team/team.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Player, Response } from '../../../@types/squad.dto.types';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { StatisticsDTO } from '../../../@types/statistics.dto';
 
 @Injectable({
@@ -23,11 +23,11 @@ export class TeamService {
 
   getStatistics(data: StatisticsDTO): Observable<string> {
     return this.http
-      .post<string>(`${this.#squadUrl}`, {
-        leagueId: data.leagueId,
-        seasonId: data.seasonId,
-        teamId: data.teamId,
-      })
-      .pipe(tap((res) => res));
+      .post(`${this.#squadUrl}`, data, { responseType: 'text' })
+      .pipe(
+        tap((res) => {
+          return res;
+        })
+      );
   }
 }
