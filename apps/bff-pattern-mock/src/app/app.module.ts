3e1c9ba75deb8d560cdae59396df0983f28ad55d
Hallison <hallison.o.b@gmail.com>
2023-06-04 18:43:03 -0300

    feat: setup nest mock api

--- apps/bff-pattern-mock/src/app/app.module.ts
+++ apps/bff-pattern-mock/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CountryService } from './src/country/country.service';
+import { LeagueService } from './src/league/league.service';
+import { TeamService } from './src/team/team.service';
+import { AuthModule } from './src/auth/auth.module';
+import { CountryModule } from './src/country/country.module';
+import { LeagueModule } from './src/league/league.module';
+import { TeamModule } from './src/team/team.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, CountryModule, LeagueModule, TeamModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CountryService, LeagueService, TeamService],
 })
 export class AppModule {}
---
+++ apps/bff-pattern-mock/src/app/src/country/country.controller.ts
@@ -0,0 +1,23 @@
+import { Country } from './@types/countrie.type';
+import { CountryService } from './country.service';
+import { DtoSearch, GetSeason } from './@types/search.type';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+@Controller()
+export class CountryController {
+  constructor(private countryService: CountryService) {}
+  @Post('countries')
+  search(@Body() value: DtoSearch): Country[] | void {
+    return this.countryService.search(value);
+  }
+
+  @Get('countries')
+  getAll(): { response: Country[] } {
+    return this.countryService.getAll();
+  }
+
+  @Get('seasons')
+  getSeasons(): GetSeason {
+    return this.countryService.getSeasons();
+  }
+}
